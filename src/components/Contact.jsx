10e4af10aca82573f539e1d42bd67028efa2ddd0
Vladmir-dev <tempestpius70@gmail.com>
2023-03-31 15:09:08 +0300

    fixed contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import leaves from '../assets/leaves.jpeg'
 
 const Contact = () => {
+
+   const bgImageStyle = {
+      // backgroundImage: `url(${leaves})`,
+      backgroundPosition: "center center",
+      backgroundRepeat:'no-repeat',
+      backgroundSize: "cover",
+      // transition: "background 0.6s ease-in-out",
+      backgroundAttachment:'fixed',
+      backgroundImage: `linear-gradient(to right, rgba(129, 216, 208, 1), rgba(0,0,0,0)), url(${leaves})`,
+    };
+
   return (
-    <div className="mt-[100px] p-4 flex flex-col justify-center items-center bg-blue-600 font-serif mb-[-50px]">
-      <div className="flex flex-col justify-center items-center md:text-[45px] text-[30px] text-white mt-[50px]">
+
+    <div style={bgImageStyle} className="mt-[100px] p-4 flex flex-col bg-gradient-to-r from-indigo-500 justify-center items-center bg-blue-600 font-serif mb-[-50px]">
+      <div className="flex flex-col font-bold  justify-center items-center md:text-[45px] text-[30px] text-black mt-[50px]">
         <h1>Make A</h1>
         <h1>Free Consultation</h1>
         <div className="flex gap-5">
@@ -14,26 +27,26 @@ const Contact = () => {
       <div className="md:mt-[100px] mt-[50px] flex gap-5 flex-col justify-center p-4 items-center md:mb-[100px]">
         <div className="flex gap-5 justify-center items-center">
           <div>
-            <input placeholder="Name" className="md:w-[500px] w-[180px] placeholder-white placeholder:text-[20px] :h-[40px] p-2 text-white border-0 bg-blue-400/50" type="text"/>
+            <input placeholder="Name" className="md:w-[500px] w-[180px] placeholder-white placeholder:text-[20px] :h-[40px] p-2 text-white border-0 bg-blue-500/70" type="text"/>
           </div>
           <div>
-            <input placeholder="Email" className="md:w-[500px] w-[180px] placeholder-white placeholder:text-[20px] md:h-[40px] p-2 text-white border-0 bg-blue-400/50" type="text"/>  
+            <input placeholder="Email" className="md:w-[500px] w-[180px] placeholder-white placeholder:text-[20px] md:h-[40px] p-2 text-white border-0 bg-blue-500/70" type="text"/>  
           </div>
         </div>
 
         <div className="flex gap-5 justify-center items-center">
           <div>
-            <input placeholder="Phone" className="md:w-[500px] w-[180px] placeholder-white placeholder:text-[20px] md:h-[40px] p-2 text-white border-0 bg-blue-400/50" type="text"/>
+            <input placeholder="Phone" className="md:w-[500px] w-[180px] placeholder-white placeholder:text-[20px] md:h-[40px] p-2 text-white border-0 bg-blue-500/70" type="text"/>
           </div>
           <div>
-            <input placeholder="YY/MM/DD" className="md:w-[500px] w-[180px] placeholder-white placeholder:text-[20px] md:h-[40px] p-2 text-white border-0 bg-blue-400/50" type="text"/>  
+            <input placeholder="YY/MM/DD" className="md:w-[500px] w-[180px] placeholder-white placeholder:text-[20px] md:h-[40px] p-2 text-white border-0 bg-blue-500/70" type="text"/>  
           </div>
         </div>
 
         <div className="flex justify-center items-center">
-           <textarea className="md:w-[1020px] md:h-[200px] align-top text-white bg-blue-400/50 laeding-normal p-[10px]">New Message</textarea>
-        </div>i
-        <div className="flex justify-center items-center mt-[20px]">
+           <textarea className="md:w-[1020px] w-[380px] md:h-[200px] h-[200px] align-top text-white bg-blue-500/70 laeding-normal p-[10px]">New Message</textarea>
+        </div>
+        <div className="flex justify-center items-center md:mt-[20px]">
           <button className="bg-white p-4 rounded-xl md:text-[25px] font-bold text-blue-500">Click To Send</button>
         </div>
       </div>
