56fde6b6579c3ef93ee684e4b1a7744e97f810cb
Vladmir-dev <tempestpius70@gmail.com>
2023-03-31 11:59:27 +0300

    fixed text area

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,24 +14,24 @@ const Contact = () => {
       <div className="md:mt-[100px] mt-[50px] flex gap-5 flex-col justify-center p-4 items-center md:mb-[100px]">
         <div className="flex gap-5 justify-center items-center">
           <div>
-            <input placeholder="Name" className="md:w-[500px] w-[180px] md:h-[40px] p-2 text-white border-0 bg-blue-400/50" type="text"/>
+            <input placeholder="Name" className="md:w-[500px] w-[180px] placeholder-white placeholder:text-[20px] :h-[40px] p-2 text-white border-0 bg-blue-400/50" type="text"/>
           </div>
           <div>
-            <input placeholder="Email" className="md:w-[500px] w-[180px] md:h-[40px] p-2 text-white border-0 bg-blue-400/50" type="text"/>  
+            <input placeholder="Email" className="md:w-[500px] w-[180px] placeholder-white placeholder:text-[20px] md:h-[40px] p-2 text-white border-0 bg-blue-400/50" type="text"/>  
           </div>
         </div>
 
         <div className="flex gap-5 justify-center items-center">
           <div>
-            <input placeholder="Phone" className="md:w-[500px] w-[180px] md:h-[40px] p-2 text-white border-0 bg-blue-400/50" type="text"/>
+            <input placeholder="Phone" className="md:w-[500px] w-[180px] placeholder-white placeholder:text-[20px] md:h-[40px] p-2 text-white border-0 bg-blue-400/50" type="text"/>
           </div>
           <div>
-            <input placeholder="YY/MM/DD" className="md:w-[500px] w-[180px] md:h-[40px] p-2 text-white border-0 bg-blue-400/50" type="text"/>  
+            <input placeholder="YY/MM/DD" className="md:w-[500px] w-[180px] placeholder-white placeholder:text-[20px] md:h-[40px] p-2 text-white border-0 bg-blue-400/50" type="text"/>  
           </div>
         </div>
 
         <div className="flex justify-center items-center">
-           <input placeholder="New Message" className="md:w-[1020px] w-[376px] h-[200px] p-2 text-white border-0 bg-blue-400/50" type="text"/>
+           <textarea className="md:w-[1020px] md:h-[200px] align-top text-white bg-blue-400/50 laeding-normal p-[10px]">New Message</textarea>
         </div>i
         <div className="flex justify-center items-center mt-[20px]">
           <button className="bg-white p-4 rounded-xl md:text-[25px] font-bold text-blue-500">Click To Send</button>
