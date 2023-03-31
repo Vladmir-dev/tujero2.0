ef3b58f555234eb9eaa4eeac7237e8ca773465b8
Vladmir-dev <tempestpius70@gmail.com>
2023-03-31 12:44:23 +0300

    fixed mobile marketing

--- src/components/Marketing.jsx
+++ src/components/Marketing.jsx
@@ -4,14 +4,14 @@ import Card from './Card'
 
 const Marketing = () => {
   return (
-    <div className="mt-[160px] mb-[50px] flex gap-20 justify-center items-center">
+    <div className="md:mt-[160px] mt-[50px] p-4 mb-[50px] flex gap-20 justify-center items-center">
       <div className="inline-block place-items-center">
-        <h1 className="text-[57px] ">SEO & Digital <br/> Marketing For Your<br/> Business</h1>
-        <p className="text-[25px] md:w-[700px] italic">We at tujero know the hardles that many business face in trying to put themselves out there. 
+        <h1 className="text-[57px] text-center md:text-start">SEO & Digital <br/> Marketing For Your<br/> Business</h1>
+        <p className="text-[25px] text-center md:text-start md:w-[700px] italic">We at tujero know the hardles that many business face in trying to put themselves out there. 
         Tujero solves that for you by advertising for you on social media and other digital platforms. Just let us handle it.
         </p>
       </div>
-      <div>
+      <div className="md:block hidden">
         <Card />
         <img src={person} alt="person"/>
       </div>
