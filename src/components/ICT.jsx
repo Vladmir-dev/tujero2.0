ecfc806be663737b4110302ae9df1808e32e7313
Vladmir-dev <tempestpius70@gmail.com>
2023-04-18 11:18:20 +0300

    added images

--- src/components/ICT.jsx
+++ src/components/ICT.jsx
@@ -67,9 +67,16 @@ const [selectedPackage, setSelectedPackage] = useState(null);
   ];
 
   return (
-    <div className="flex justify-center w-full  mt-[70px] p-4 gap-20 items-start">
-      <div className="hidden md:block md:w-[500px]">
-       <Pricing packages={packages}/> 
+    <div className="flex flex-row justify-center w-full  mt-[70px] p-4 gap-20 items-center">
+      <div className="hidden md:block p-4">
+        <div className="flex justify-center items-center gap-16">
+        <div className="flex flex-col gap-10">
+          <img src={slide1} alt="image1" className="rounded-full w-[300px] h-[300px] shadow-2xl"/>
+          <img src={slide2} alt="image2" className="rounded-full w-[250px] h-[250px] shadow-2xl"/>
+        </div>
+        
+        <img src={coding} alt="coding" className="rounded-full w-[400px] h-[400px] shadow-2xl"/>
+      </div>
       </div>
 
         <div className="flex justify-center items-center  h-full p-4">
@@ -79,7 +86,7 @@ const [selectedPackage, setSelectedPackage] = useState(null);
              <div className="flex justify-start items-start w-full ">
               <h2 className="text-white bg-blue-400 float-left mr-[20px] p-5 text-bold md:h-[40px] md:w-[40px] rounded-full text-[30px] justify-center items-center flex rounded-full">1</h2>
                 <div className="w-full inline-block">
-                  <h2 className="text-[30px]  font-bold text-blue-500">Web Development</h2>
+                  <h2 className="text-[30px] text-blue-500">Web Development</h2>
                     <p className="flex-wrap md:w-[400px]" >whether you're looking for a simple website to showcase your business, an e-commerce platform to boost your sales, or a complex software solution to streamline your operations, we have the expertise to help your business succeed in today's digital age</p>
                 </div>
             </div>
@@ -90,7 +97,7 @@ const [selectedPackage, setSelectedPackage] = useState(null);
            <div className="flex justify-start items-start w-full ">
            <h2 className="text-white bg-blue-400 float-left mr-[20px] p-5 text-bold md:h-[40px] md:w-[40px] rounded-full text-[30px] justify-center items-center flex rounded-full">2</h2>
             <div className="w-full inline-block">
-           <h2 className="text-[30px]  font-bold text-blue-500">App Development</h2>
+           <h2 className="text-[30px]  text-blue-500">App Development</h2>
             <p className="flex-wrap md:w-[400px]" >Our team of expert developers specializes in creating mobile apps that enhance customer engagement, increase brand visibility, and drive business growth.Our app development services are tailored to meet the specific needs of your business, whether you're in healthcare, education, retail, or finance. We take the time to understand your unique requirements and develop customized solutions that solve your business challenges and provide a seamless user experience</p>
            </div>
            </div>
@@ -101,7 +108,7 @@ const [selectedPackage, setSelectedPackage] = useState(null);
              <div className="flex justify-start items-start w-full ">
               <h2 className="text-white bg-blue-400 float-left mr-[20px] p-5 text-bold md:h-[40px] md:w-[40px] rounded-full text-[30px] justify-center items-center flex rounded-full">3</h2>
                 <div className="w-full inline-block">
-                  <h2 className="text-[30px]  font-bold text-blue-500">Artificial Intelligence & Machine Learning</h2>
+                  <h2 className="text-[30px] text-blue-500">Artificial Intelligence & Machine Learning</h2>
                     <p className="flex-wrap md:w-[400px]" >Our AI and machine learning services are tailored to meet the specific needs of your business, whether you're in healthcare, finance, e-commerce, or any other industry. We take the time to understand your unique requirements and develop customized solutions that solve your business challenges and drive growth</p>
                 </div>
             </div>
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -12,16 +12,16 @@ const Pricing = ({packages}) => {
 
   return (
     <div>
-      <h2 className="text-[37px] text-center m-[10px] text-blue-500">Choose a Package</h2>
-      <div className="flex flex-col gap-8 mt-[50px]">
+      <h2 className="text-[37px] text-center m-[ 10px] text-blue-500">Choose a Package</h2>
+      <div className="flex mt-[50px] gap-5">
         {packages.map((pkg) => (
-          <div key={pkg.type} onClick={() => handlePackageSelect(pkg)} className="border-[2px] border-solid border-blue-400 rounded-md hover:bg-blue-500 text-purple-500 hover:text-white duration-500 p-2 shadow-md">
-            <h3 className="text-[25px] font-bold">{pkg.name}</h3>
+          <div key={pkg.type} onClick={() => handlePackageSelect(pkg)} className="rounded-md hover:bg-blue-500 bg-black/70 text-blue-500 hover:text-white duration-500 p-2 shadow-md flex justify-center items-center">
+            <h3 className="text-[25px] text-center font-bold">{pkg.name}</h3>
           </div>
         ))}
       </div>
       {selectedPackage && (
-        <div className="flex flex-col shadow-xl  rounded-md justify-center items-center mt-[50px] border-[2px] border-solid p-[10px]">
+        <div className="flex flex-col shadow-xl py-6 rounded-md justify-center items-center mt-[50px] border-[2px] border-solid p-[10px]">
           <h3 className="text-[30px] text-blue-500">{selectedPackage.name}</h3>
           <p className="italic">This package offers</p>
           <p className="text-[27px] font-bold">{selectedPackage.price} UGX</p>
